d46f906b852d2a05f8237688a2d8a4fd2ff5a723
Adrien Lindeberg <adrien.lindeberg@gmail.com>
2024-01-08 14:03:24 +0100

    ajout requirement + graph et top 5 dans flask et barre nav

---
+++ Projet/flask/.env/app.py
@@ -0,0 +1,170 @@
+from flask import Flask, render_template
+from flask_pymongo import PyMongo
+import plotly.express as px
+from io import BytesIO
+
+app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/basket"
+mongo = PyMongo(app).db.nba_player
+
+@app.route("/")
+def home():
+    return render_template("home.html", image = "/static/images/image1.jpg")
+
+
+@app.route("/top5")
+def top5():
+    top_MJ_players = mongo.find().sort("MJ", -1).limit(5)
+    top_MJ_players_data = [(player["player"], player["MJ"]) for player in top_MJ_players]
+
+    top_min_players = mongo.find().sort("minutes", -1).limit(5)
+    top_min_players_data = [(player["player"], player["minutes"]) for player in top_min_players]
+
+
+    top_tir_players = mongo.find({"MJ": {"$gte": 20}}).sort("tir", -1).limit(5)
+    top_tir_players_data = [(player["player"], player["tir"]) for player in top_tir_players]
+
+    top_tir3_pts_players = mongo.find({"MJ": {"$gte": 20}}).sort("tir_3_pts", -1).limit(5)
+    top_tir_3_pts_players_data = [(player["player"], player["tir_3_pts"]) for player in top_tir3_pts_players]
+
+    top_lf_players = mongo.find({"MJ": {"$gte": 20}}).sort("lf", -1).limit(5)
+    top_lf_players_data = [(player["player"], player["lf"]) for player in top_lf_players]
+
+    top_rb_off_players = mongo.find({"MJ": {"$gte": 20}}).sort("rb_off", -1).limit(5)
+    top_rb_off_players_data = [(player["player"], player["rb_off"]) for player in top_rb_off_players]
+
+    top_rb_def_players = mongo.find({"MJ": {"$gte": 20}}).sort("rb_df", -1).limit(5)
+    top_rb_def_players_data = [(player["player"], player["rb_df"]) for player in top_rb_def_players]
+
+    top_rb_players = mongo.find({"MJ": {"$gte": 20}}).sort("rb", -1).limit(5)
+    top_rb_players_data = [(player["player"], player["rb"]) for player in top_rb_players]
+
+    top_pd_players = mongo.find({"MJ": {"$gte": 20}}).sort("pd", -1).limit(5)
+    top_pd_players_data = [(player["player"], player["pd"]) for player in top_pd_players]
+
+    top_bp_players = mongo.find({"MJ": {"$gte": 20}}).sort("bp", -1).limit(5)
+    top_bp_players_data = [(player["player"], player["bp"]) for player in top_bp_players]
+
+    top_inter_players = mongo.find({"MJ": {"$gte": 20}}).sort("inter", -1).limit(5)
+    top_inter_players_data = [(player["player"], player["inter"]) for player in top_inter_players]
+
+    top_ct_players = mongo.find({"MJ": {"$gte": 20}}).sort("ct", -1).limit(5)
+    top_ct_players_data = [(player["player"], player["ct"]) for player in top_ct_players]
+
+    top_fte_players = mongo.find({"MJ": {"$gte": 20}}).sort("fte", -1).limit(5)
+    top_fte_players_data = [(player["player"], player["fte"]) for player in top_fte_players]
+
+    top_pts_players = mongo.find({"MJ": {"$gte": 20}}).sort("pts", -1).limit(5)
+    top_pts_players_data = [(player["player"], player["pts"]) for player in top_pts_players]
+
+    return render_template("classement.html", top_MJ_players=top_MJ_players_data,
+                           top_min_players=top_min_players_data, top_tir_players=top_tir_players_data,
+                           top_tir_3_pts_players=top_tir_3_pts_players_data, top_lf_players=top_lf_players_data,
+                           top_rb_off_players=top_rb_off_players_data, top_rb_def_players=top_rb_def_players_data,
+                           top_rb_players=top_rb_players_data, top_pd_players=top_pd_players_data,
+                           top_bp_players=top_bp_players_data, top_inter_players=top_inter_players_data,
+                           top_ct_players=top_ct_players_data, top_fte_players=top_fte_players_data,
+                           top_pts_players=top_pts_players_data)
+
+
+@app.route("/graphs")
+def user():
+
+    results = mongo.aggregate([
+        {
+            "$group": {
+                "_id": "$team",
+                "nb": {"$sum": {"$cond": [{"$gte": ["$pts", 20]}, 1, 0]}}
+            }
+        }
+    ])
+
+    categories = []
+    counts = []
+
+    for result in results:
+        categories.append(result["_id"])
+        counts.append(result["nb"])
+
+    fig = px.bar(x=categories, y=counts)
+    fig.update_traces(marker_color="#0487B5")
+    fig.update_layout(
+        title="Nombre de joueurs ayant plus de 20pts par franchise",
+        xaxis_title="Franchise",
+        yaxis_title="Nombre",
+        title_x=0.5
+    )
+
+    graph_1 = fig.to_html(full_html=False)
+
+    cur = list(mongo.find())
+
+    minj = []
+    pts = []
+
+    for result in cur:
+        minj.append(result["minutes"])
+        pts.append(result["pts"])
+
+    fig = px.scatter(x=minj, y=pts)
+    fig.update_traces(marker_color="#0487B5")
+    fig.update_layout(
+        title="Nombre de points marqués en fonction du temps de jeu",
+        xaxis_title="temps de jeu",
+        yaxis_title="Points",
+        title_x=0.5
+    )
+
+    graph_2 = fig.to_html(full_html=False)
+
+
+    size = [result["size"] for result in cur]
+
+    fig = px.histogram(x=size, nbins=15)
+    fig.update_traces(marker_color="#0487B5", opacity=0.7, marker_line_color='black', marker_line_width=0.5)
+    fig.update_layout(
+        title="Tailles des joueurs en 2023-2024",
+        title_x=0.5,
+        xaxis_title="Tailles",
+        yaxis_title="Nombre",
+        bargap=0.1,
+        barmode='overlay'
+    )
+
+    graph_3 = fig.to_html(full_html=False)
+
+
+    weight = [result["weight"] for result in cur]
+
+    fig = px.histogram(x=weight, nbins=15)
+    fig.update_traces(marker_color="#0487B5", opacity=0.7, marker_line_color='black', marker_line_width=0.5)
+    fig.update_layout(
+        title="Poids des joueurs en 2023-2024",
+        title_x=0.5,
+        xaxis_title="Poids",
+        yaxis_title="Nombre",
+        bargap=0.1,
+        barmode='overlay'
+    )
+
+    graph_4 = fig.to_html(full_html=False)
+
+
+    nationality = [entry.strip() for result in cur for entry in result["nationality"]]
+
+    fig = px.pie(names=nationality)
+    fig.update_traces(opacity=0.7, textinfo='none', hovertemplate='%{label}: %{percent:.1%}')
+    fig.update_layout(
+        title="Répartition de la nationalité des joueurs en 2023-2024",
+        title_x=0.5,
+        legend_title_text='Nationalité: '
+    )
+
+    graph_5 = fig.to_html(full_html=False)
+
+    return render_template('chart.html', graph_1=graph_1, graph_2=graph_2, graph_3=graph_3,
+                           graph_4=graph_4, graph_5=graph_5)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# data_engineer
+**Exploration des Statistiques des Joueurs NBA 2023-2024 : Projet data engineer**
+
+La National Basketball Association (NBA) représente aujourd'hui le plus haut niveau de compétition de basketball dans le monde. 
+Avec de plus en plus de passionées qui la suit au quotidien, le niveau de spectacle ne fait qu'augmenter avec des joueurs qui sortent de 
+plus en plus d'énormes performance. Ces statistiques nous offrent un aperçu unique de la manière dont chaque joueur contribue à la saison de son équipe et influence le cours des matchs.
+
+Notre projet se consacre à la découverte des statistiques des joueurs de la saison 2023-2024. En utilisant des techniques de scrapping de données avancées avec Scrapy, nous collecterons les données relatives à chaque joueur, des points marqués aux rebonds, en passant par les passes décisives et bien plus encore. Ces informations seront ensuite stockées de manière organisée dans une base de données MongoDB.
+Notre projet intègrera également la création d'une application web interactive à l'aide de Flask. Cette application permettra de suivre l'évolution du niveau des joueurs au fil de la saison, et de dégager des tendances significatives.
+
+
+***Comment récuperer notre projet avec docker:***
+
+
+
+
+
+***Scrapping des données:***
+
+Dans un premier temps nous avons cherché un site où l'on pourrait récupérer nos données. Nous sommes pas tout de suite partie sur la NBA.
+Notre première idée était le site de l'ATPtour et récupérer différents classement sur le tennis (meilleures joueurs, serveurs, retourneurs...). Cependant le site de l'ATP n'était scrappable et nous nous sommes tourné vers la NBA. Nous avons trouvé un site qui regroupé les stats des franchises et des joueurs et après réflexion, nous sommes partis sur les stats de tout les joueurs NBA de l'année 2023-2024 car cela représentait une plus grosse quantité de données et donc un projet plus ambitieux. Le domaine de notre site est: basketusa.com.
+
+Les données des joueurs sont organisées en fonction de leurs franchises, nécessitant ainsi des requêtes distinctes sur 30 URL différentes, une par franchise de la NBA. Pour chaque joueur, nous récupérons ses statistiques telles que le temps de jeu moyen, le nombre moyen de points marqués, ou le nombre moyen de fautes commises. Ensuite, nous effectuons une nouvelle requête pour chaque joueur afin d'accéder à une page contenant des informations plus détaillées sur le joueur, telles que sa taille, son poids, son âge, etc. Cela nous fait au total un nombre de requêtes sur plus de 500 urls différentes.
+
+Une fois les données récupérées, nous les avons mit en forme grâce à des pipelines. Nous avont mis en place trois pipelines différentes par l'aquelle chaque item passé. La première pipeline met sous la forme textuelle désiré tout les items puis la deuxième transforme certaines champs en int ou float (age, nombre de points par match...). La dernière pipeline automatise l'ajout de tout nos items dans une database mongo.
+
+La récupération des données se fait par la commande *$scrapy crawl nba_player* dans un powershell dans le repertoire du spider.
+On retrouve les données dansla database basket puis la collection nba_player dans mongoDB.
